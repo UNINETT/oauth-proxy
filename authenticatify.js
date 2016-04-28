dfe3cb4056b228faaf676e5508cc723a5f46859b
Jørn Åne <jorn.dejong@uninett.no>
2016-04-28 09:56:54 +0200

    Add "next" function to authenticatify handler.

--- authenticatify.js
+++ authenticatify.js
@@ -3,13 +3,13 @@ const passport = require('passport');
 module.exports = authenticatify;
 
 function authenticatify(func, strategy) {
-	return function(req, res) {
+	return function(req, res, next) {
 		var session = req.session;
 		if ('passport' in session) {
 			var token = session.passport.user.accessToken;
 			req.headers['authorization'] = 'Bearer ' + token; // add the bearer token
 			delete req.headers['cookie'];
-			return func(req, res);
+			return func(req, res, next);
 		} else {
 			session.oauth_proxy = {url:req.url};
 			return passport.authenticate(strategy) (req, res);
