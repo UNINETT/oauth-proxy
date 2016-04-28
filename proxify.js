dd9daa7ab572517164ac5063a546ceda67c9b154
Jørn Åne <jorn.dejong@uninett.no>
2016-04-28 10:12:59 +0200

    Clear session when receiving a 401 from upstream.

--- proxify.js
+++ proxify.js
@@ -3,9 +3,24 @@ const httpProxy = require('http-proxy');
 module.exports = proxify;
 
 function proxify(target) {
-	return function(req, res) {
-		const proxy = httpProxy.createProxyServer({});
+	const proxy = httpProxy.createProxyServer({});
 
+	proxy.on('proxyRes', function (proxyRes, req, res) {
+		// If we get a 401 back, our token is no longer valid.
+		// The proxy doesn't know how to fix it,
+		// so we'll just remove the session.
+		if (proxyRes.statusCode == 401) {
+			for(var key in req.session)
+				if (key !== 'cookie')
+					delete req.session[key];
+			// Now redirect to self so that the client will re-attempt
+			// the request without a valid session.
+			proxyRes.statusCode = 307;
+			proxyRes.headers['Location'] = req.url;
+		}
+	});
+
+	return function(req, res) {
 		var parsedTarget = require('url').parse(target);
 		req.headers['host'] = parsedTarget.host; // hack to fix TLS-cert check
 
